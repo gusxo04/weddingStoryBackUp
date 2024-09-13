de715f0da51421e99cf75fab7be0957c24ad86cc
gusxo04 <gusxo004@naver.com>
2024-09-13 18:26:56 +0900

    시작

--- wedding_story_front/src/component/convention/ConventionMain.js
+++ wedding_story_front/src/component/convention/ConventionMain.js
@@ -1,16 +1,23 @@
 import axios from "axios";
 import "./convention.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 const ConventionMain = () => {
 
   const backServer = process.env.REACT_APP_BACK_SERVER;
 
+  const [convention, setConvention] = useState({});
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
   useEffect(() => {
     axios.get(`${backServer}/convention`)
     .then(res => {
-      console.log(res)
+      console.log(res);
+      setConvention(res.data.convention);
+      setStartDate(res.data.convention);
+      setEndDate(res.data.endDate);
     })
     .catch(err => {
       console.error(err); 
@@ -20,7 +27,16 @@ const ConventionMain = () => {
   return (
     <div className="convention-wrap">
       <div className="convention-container">
-
+        {convention ?
+        endDate < 0 ? 
+        "박람회 없음" 
+        :
+        startDate > 0 ? 
+        "신청 가능" 
+        : 
+        "박람회 있음"
+        :
+        ""}
       </div>
     </div>
   )
