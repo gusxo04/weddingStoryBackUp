c3777c30ef9b187efbcfbccc7e804e3479f016cb
gusxo04 <gusxo004@naver.com>
2024-09-17 19:11:36 +0900

    프리뷰 제작3

--- wedding_story_front/src/component/convention/Preview.js
+++ wedding_story_front/src/component/convention/Preview.js
@@ -1,3 +1,4 @@
+import { Viewer } from "@toast-ui/react-editor";
 
 
 const Preview = (props) => {
@@ -20,47 +21,77 @@ const Preview = (props) => {
       {imgStyle === 1 ? 
       <div className="back-main-wrap">
         <div className="back-image-zone">
-          {/* <img id="back-image" src={showImage} /> */}
-          <div style={{backgroundImage: `url(${showImage})`,backgroundSize: 'contain',
-        backgroundRepeat: 'no-repeat',
-        height: '1000px',
-        width: '100vw',}} id="back-image" ></div>
-        </div>
-      </div>
-
-      :
-
-      <div className="preview-main-wrap">
-        <div className="main-image-zone">
-          <img id="main-image" src={showImage} />
+          <img id="back-image" src={showImage} />
         </div>
 
-        <div className="main-info-zone">
-          <div className="main-info-title">
-            <h2>{conventionTitle}</h2>
+        <div className="back-inner-img-zone">
+          <div className="back-info-title">
+            <div className="back-title">{conventionTitle}</div>
           </div>
 
-          <div className="main-info-date">
-            <div>일정 : {conventionStart} ~ {conventionEnd}</div>
-          </div>
-
-          <div className="main-info-time">
-            <div>시간 : {conventionTime}</div>
+          <div className="back-info-date">
+            <div>일정 : {conventionStart} ~ {conventionEnd} <br /> 시간 : {conventionTime}</div>
           </div>
           
-          <div className="main-info-price">
+          <div className="back-info-price">
             <div>가격 : {conventionPrice}원</div>
           </div>
 
-          <div className="main-info-limit">
+          <div className="back-info-limit">
             <div>정원 : {conventionLimit}명</div>
+          </div>
+
+          <div className="back-info-content">
+            {conventionContent ? 
+            <Viewer initialValue={conventionContent} />
+            : 
+            ""}
+          </div>
+
+        </div>
+        
+      </div>
+
+      :
+      <>
+        <div className="preview-main-wrap">
+          <div className="main-image-zone">
+            <img id="main-image" src={showImage} />
+          </div>
+
+          <div className="main-info-zone">
+            <div className="main-info-title">
+              <h2>{conventionTitle}</h2>
+            </div>
+
+            <div className="main-info-date">
+              <div>일정 : {conventionStart} ~ {conventionEnd}</div>
+            </div>
+
+            <div className="main-info-time">
+              <div>시간 : {conventionTime}</div>
+            </div>
+            
+            <div className="main-info-price">
+              <div>가격 : {conventionPrice}원</div>
+            </div>
+
+            <div className="main-info-limit">
+              <div>정원 : {conventionLimit}명</div>
+            </div>
+
           </div>
 
 
         </div>
-
-        
-      </div>
+          
+        <div className="preview-main-content">
+          {conventionContent ? 
+          <Viewer initialValue={conventionContent} />
+          : 
+          ""}
+        </div>
+      </>
       }
     </div>
   )
