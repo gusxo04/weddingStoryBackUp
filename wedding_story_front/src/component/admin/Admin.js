40307da2042a18ba77d6cf0cc5ef3e1932d517c5
KingyeonU <kattack0702@naver.com>
2024-09-19 09:48:19 +0900

    commit

--- wedding_story_front/src/component/admin/Admin.js
+++ wedding_story_front/src/component/admin/Admin.js
@@ -6,7 +6,24 @@ const Admin = () => {
   return (
     <div>
       <h3>관리자 페이지</h3>
+      <a>|</a>
+      <Link to="/sales">회원</Link>
+      <a>|</a>
+      <Link to="/sales">업체</Link>
+      <a>|</a>
+      <Link to="/sales">광고</Link>
+      <a>|</a>
       <Link to="/sales">매출</Link>
+      <a>|</a>
+      <a>|</a>
+      <Link to="/sales">공지사항</Link>
+      <a>|</a>
+      <a>|</a>
+      <Link to="/sales">Q&A</Link>
+      <a>|</a>
+      <a>|</a>
+      <Link to="/sales">관리자</Link>
+      <a>|</a>
     </div>
   );
 };
