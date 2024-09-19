795fab04d241eb25c86edc6ab87350ab1d3f433f
KingyeonU <kattack0702@naver.com>
2024-09-19 14:03:55 +0900

    company 파일 생성

--- wedding_story_front/src/component/admin/CompanyControll.js
+++ wedding_story_front/src/component/admin/CompanyControll.js
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import "./companyControll.css";
+import axios from "axios";
 const CompanyControll = () => {
+  const backServer = process.env.REACT_APP_BACK_SERVER;
+  const [companyList, setCompanyList] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${backServer}/admin/company`)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          setCompanyList(Object.values(res.data));
+        } else {
+          console.error("업체 없음");
+          setCompanyList([]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        console.log("조회 에러");
+        setCompanyList([]);
+      });
+  }, []);
   return (
     <div>
       <a>업체관리</a>
