baa4280a8e0f5bb763b3a6499b27737a93b2b8b0
seong <tpdud3449@naver.com>
2024-09-19 11:14:40 +0900

    header 디자인

---
+++ wedding_story_front/src/component/common/Header.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import "./common.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <div className="header-link">
+          <Link to="#">로그인</Link>
+          <span>|</span>
+          <Link to="#">회원가입</Link>
+          <span>|</span>
+          <Link to="#">고객센터</Link>
+        </div>
+        <div className="header-logo">
+          <img src="../../../public/image/main_logo.png" />
+        </div>
+        <div className="header-nav">
+          <ul className="nav-main">
+            <li className="nav-suv">
+              <span class="material-icons">menu</span>
+              <Link to="#">My Wedding</Link>
+            </li>
+            <li>
+              <Link to="#">홈</Link>
+            </li>
+            <li>
+              <Link to="#">웨딩홀</Link>
+            </li>
+            <li>
+              <Link to="#">스드메</Link>
+            </li>
+            <li>
+              <Link to="#">박람회</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
