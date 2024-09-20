36b00fb2d53eb70e900f817858df1db6563a1384
rlawl135 <rlawl135@gmail.com>
2024-09-20 13:49:10 +0900

    커밋1

--- wedding_story_front/src/component/product/ProductMain.js
+++ wedding_story_front/src/component/product/ProductMain.js
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import ProductAllView from "./ProductAllView";
 import "./product.css";
 import Counsel from "../marriageCounseling/Counsel";
 import ProductPay from "./ProductPay";
+import ProductReview from "./ProductReview";
+import ProductAllList from "./ProductAllList";
+import ProductInfo from "./ProductInfo";
 const ProductMain = () => {
   return (
     <Routes>
-      <Route path="list" element={<ProductAllView />} />
+      <Route path="list" element={<ProductAllList />} />
       <Route path="counsel" element={<Counsel />} />
       <Route path="pay" element={<ProductPay />} />
+      <Route path="review" element={<ProductReview />} />
+      <Route path="info" element={<ProductInfo />} />
     </Routes>
   );
 };
