eadfe61314e44c0200094820a13c92822c06f2d8
seong <tpdud3449@naver.com>
2024-09-22 18:47:41 +0900

    회원가입 state생성 및 back 패키지 생성

---
+++ wedding_story_back/src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -0,0 +1,12 @@
+package kr.co.iei.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CrossOrigin("*")
+@Controller
+@RequestMapping(value="/")
+public class MemberController {
+
+}
---
+++ wedding_story_back/src/main/java/kr/co/iei/member/model/dto/MemberDTO.java
@@ -0,0 +1,30 @@
+package kr.co.iei.member.model.dto;
+
+import java.sql.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Alias(value="member")
+public class MemberDTO {
+	
+	private int memberNo;
+	private String companyCode;
+	private String memberCode;
+	private int memberType;
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String memberGender;
+	private String memberPhone;
+	private String memberEmail;
+	private String partnerName;
+	private String partnerPhone;
+	private Date enrollDate;
+}
