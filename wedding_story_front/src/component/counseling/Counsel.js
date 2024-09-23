a285dcb8376ecd06899b17238df9f513f2376a5a
rlawl135 <rlawl135@gmail.com>
2024-09-24 07:43:24 +0900

    커밋

--- wedding_story_front/src/component/counseling/Counsel.js
+++ wedding_story_front/src/component/counseling/Counsel.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import ToastEditor from "../utils/ToastEditor";
 import "./counsel.css";
 import { useRecoilState } from "recoil";
 
