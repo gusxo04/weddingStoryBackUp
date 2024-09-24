a8b9466c16c290d767901630462fe6128375538f
KingyeonU <kattack0702@naver.com>
2024-09-24 17:46:18 +0900

    commit

--- wedding_story_back/src/main/java/kr/co/iei/admin/controller/AdminController.java
+++ wedding_story_back/src/main/java/kr/co/iei/admin/controller/AdminController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.iei.admin.model.dto.TestCompanyDTO;
-import kr.co.iei.admin.model.dto.TestMemberDTO;
 import kr.co.iei.admin.model.service.AdminService;
 import kr.co.iei.util.FileUtils;
 
