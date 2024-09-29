0bb1551384aaf8d7391728533c0fbc9d6d3cccdc
gusxo04 <gusxo004@naver.com>
2024-09-29 18:28:24 +0900

    박람회 작성폼 날짜관련 DatePicker로 변경

--- wedding_story_front/src/component/convention/InsertConvention.js
+++ wedding_story_front/src/component/convention/InsertConvention.js
@@ -40,11 +40,14 @@ const InsertConvention = () => {
     setConventionTime(conventionStartTime+" ~ "+conventionEndTime);
   }, [conventionStartTime, conventionEndTime]);
 
+  console.log(conventionStart);
+
 
   
 
 
   const writeTest = () => {
+    console.log(conventionStart);
     let isTest = true;
     const titleReg = /^.{1,100}$/;
     const contentReg = /^.{1,1300}$/;
@@ -161,7 +164,9 @@ const InsertConvention = () => {
     return false;
   }
   
+
   const compareTime = () => {
+
     // 시간 비교
     if(conventionStartTime === "" || conventionEndTime === "") return false;
     const startHour = parseInt(conventionStartTime.split(":")[0]);
