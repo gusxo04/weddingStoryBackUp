10ba5f340337027271c94ae0104da7a69641073e
gusxo04 <gusxo004@naver.com>
2024-09-29 16:37:24 +0900

    박람회 티켓 개인정보동의 추가

--- wedding_story_front/src/component/convention/BuyTicket.js
+++ wedding_story_front/src/component/convention/BuyTicket.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import Swal from "sweetalert2";
 import { loginNoState } from "../utils/RecoilData";
+import PersonalPolicy from './../utils/PersonalPolicy';
 
 const BuyTicket = (props) => {
 
@@ -29,6 +30,12 @@ const BuyTicket = (props) => {
   const [memberPhone, setMemberPhone] = useState("");
   const [memberEmail, setMemberEmail] = useState("");
 
+  const [showType, setShowType] = useState(true);
+  const [checkable, setCheckable] = useState(false);
+
+  const checkRef = useRef(null);
+  const checkEmailRef = useRef(null);
+
   useEffect(() => {
     console.log("uef");
     axios.get(`${backServer}/convention/get/memberInfo/${memberNoState}`)
@@ -49,9 +56,17 @@ const BuyTicket = (props) => {
   const submit = () => {
     dateMsgRef.current.style.display = "none";
     personalMsgRef.current.style.color = "black";
-    if(!personalRef.current.checked || !selectDate){
+    checkEmailRef.current.textContent = "알림받을 이메일을 입력해주세요";
+    checkEmailRef.current.style.color = "black";
+    const emailRegex = /^[a-zA-Z]{3,30}$/;
+
+    if(!personalRef.current.checked || !selectDate || (noticeEmail.trim() !== "" && !emailRegex.test(noticeEmail))){
       if(!selectDate) dateMsgRef.current.style.display = "flex";
-      if(!personalRef.current.checked) personalMsgRef.current.style.color = "red";
+      if(!personalRef.current.checked) personalMsgRef.current.style.color = "var(--red2)";
+      if(noticeEmail.trim() !== "" && !emailRegex.test(noticeEmail)) {
+        checkEmailRef.current.textContent = "정확한 이메일을 입력해주세요";
+        checkEmailRef.current.style.color = "var(--red2)";
+      }
       return false;
     }
 
@@ -130,6 +145,9 @@ const BuyTicket = (props) => {
     });
     
   }
+
+
+  
   
   return (
     <div className="convention-member-alert-wrap" id="convention-close-screen" onClick={closeAlert}>
@@ -137,7 +155,7 @@ const BuyTicket = (props) => {
 
         <div className="convention-email-wrap">
           <div className="convention-email-info-wrap">
-            <label htmlFor="email"><span>알림받을 이메일을 입력해주세요</span></label>
+            <label htmlFor="email" ref={checkEmailRef} >알림받을 이메일을 입력해주세요</label>
           </div>
 
           <div className="convention-email-input-wrap">
@@ -158,37 +176,52 @@ const BuyTicket = (props) => {
           </div>
 
           <div className="convention-date-msg" style={{display:"none"}} ref={dateMsgRef} >
-            <span style={{color:"red"}}>날짜를 선택해주세요</span>
+            <span style={{color:"var(--red2)"}}>날짜를 선택해주세요</span>
           </div>
           
         </div>
         
         <div className="convention-date-wrap">
-          {conventionShowDate.map((date,index) => {
-            const selectDate = (e) => {
-              setSelectDate(e.target.value);
-            }
-            return (
-              <div key={"date-"+index} className="convention-inner-date">
-                <label htmlFor={"date-label-"+index+1}>
-                  <span>{date.toLocaleDateString().slice(0,-1).trim().split(" ").join("").split('.').join('-')}</span>
-                  <span id="small">({date.toLocaleDateString('ko-kr', {weekday: 'short'})})</span>
-                </label>
-                <input type="radio" id={"date-label-"+index+1} name="date-radio" 
-                value={date.toLocaleDateString().slice(0,-1).trim().split(" ").join("").split('.').join('-')} 
-                onClick={selectDate}
-
-                />
-              </div>
-            )
-          })}
+        {showType ? 
+          <>
+            {conventionShowDate.map((date,index) => {
+              const selectDate = (e) => {
+                setSelectDate(e.target.value);
+              }
+              return (
+                <div key={"date-"+index} className="convention-inner-date">
+                  <label htmlFor={"date-label-"+index+1}>
+                    <span>{date.toLocaleDateString().slice(0,-1).trim().split(" ").join("").split('.').join('-')}</span>
+                    <span id="small">({date.toLocaleDateString('ko-kr', {weekday: 'short'})})</span>
+                  </label>
+                  <input type="radio" id={"date-label-"+index+1} name="date-radio" 
+                  value={date.toLocaleDateString().slice(0,-1).trim().split(" ").join("").split('.').join('-')} 
+                  onClick={selectDate}
+                  
+                  />
+                </div>
+              )
+            })}
+          </>
+          : 
+          <PersonalPolicy />
+        }
         </div>
 
         <div className="convention-personal-info-wrap">
-          <span>!</span>
+          <span className="material-icons" ref={checkRef} id="convention-personal-info" onClick={() => {
+            checkRef.current.style.color = "black";
+            setShowType(!showType);
+            setCheckable(true);
+          }}>info</span>
           {/* ! 누르면 약관 보여주기 언젠간하겠지 */}
           <label htmlFor="personal" ref={personalMsgRef} >이용약관및 개인정보 수집/이용 동의</label>
-          <input type="checkbox" id="personal" ref={personalRef} />
+          <input type="checkbox" id="personal" ref={personalRef} onClick={() => {
+            if(checkable) return;
+            // 옆에 정보 보기는 한 번은 눌러야 체크 가능함
+            personalRef.current.checked = false;
+            checkRef.current.style.color = "var(--red2)";
+          }} />
         </div>
 
         <div className="convention-btn-wrap">
@@ -197,6 +230,8 @@ const BuyTicket = (props) => {
           }}>취소</button>
           <button className="convention-submit-btn" onClick={submit}>결제</button>
         </div>
+
+
         
       </div>
     </div>
--- wedding_story_front/src/component/convention/RefundTicket.js
+++ wedding_story_front/src/component/convention/RefundTicket.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import Swal from "sweetalert2";
 import { loginNoState } from "../utils/RecoilData";
@@ -15,7 +15,9 @@ const RefundTicket = (props) => {
   } = props;
 
   const [memberNoState, setMemberNoState] = useRecoilState(loginNoState);
-  // const [memberNoState, setMemberNoState] = useState(2);
+  const [refundReason, setRefundReason] = useState("");
+
+  const reasonRef = useRef(null);
   
   class CancelPay extends React.Component {
     cancelPay = () => {
@@ -30,7 +32,7 @@ const RefundTicket = (props) => {
           ticketNo : payment.ticketNo,
           merchantUid: payment.merchantUid, // 주문번호
           cancelRequestAmount: payment.payPrice, // 환불금액
-          reason: "테스트", // 환불사유
+          reason: refundReason, // 환불사유
           // refundHolder: "홍길동", // [가상계좌 환불시 필수입력] 환불 수령계좌 예금주
           // refundBank: "04", // [가상계좌 환불시 필수입력] 환불 수령계좌 은행코드(예: KG이니시스의 경우 신한은행은 88번)
           // refundAccount: "3333-4444-5555-6666", // [가상계좌 환불시 필수입력] 환불 수령계좌 번호
@@ -64,7 +66,7 @@ const RefundTicket = (props) => {
       });
     };
     render() {
-      return <button id="refund-btn" onClick={this.cancelPay}>환불하기</button>;
+      return <button id="refund-btn" className="main-btn" onClick={this.cancelPay}>환불하기</button>;
     }
   }
   
@@ -72,7 +74,32 @@ const RefundTicket = (props) => {
   return (
     <div className="convention-member-alert-wrap convention-refund-wrap" id="convention-close-screen" onClick={closeAlert}>
       <div className="convention-refund-alert-wrap">
-        <CancelPay />
+        <div className="convention-refund-title df-basic">
+          <span>박람회 티켓 환불</span>
+        </div>
+
+        <div className="convention-refund-content">
+          <div className="convention-refund-span df-basic">
+            <span>티켓 환불 사유</span>
+          </div>
+
+          <div className="convention-refund-reason df-basic">
+            <textarea id="refund-text" value={refundReason} ref={reasonRef} onChange={(e) => {
+              setRefundReason(e.target.value);
+              if(reasonRef.current.scrollHeight > reasonRef.current.clientHeight){
+                reasonRef.current.style.borderRadius = "20px 0px 0px 20px";
+              }
+              else if(reasonRef.current.scrollHeight == reasonRef.current.clientHeight){
+                reasonRef.current.style.borderRadius = "20px";
+              }
+            }}></textarea>
+          </div>
+          
+        </div>
+
+        <div className="convention-refund-btn df-basic">
+          <CancelPay />
+        </div>
 
       </div>
     </div>
