d003ead3d8f2db040d42150cb22c3ae6848e05ae
kdy <doyep1023@naver.com>
2024-10-10 20:45:21 +0900

    수정

--- wedding_story_front/src/component/company/CompanyMain.js
+++ wedding_story_front/src/component/company/CompanyMain.js
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import CompanyJoin from "./CompanyJoin";
 import "./company.css";
 import CompanyInfo from "./CompanyInfo";
@@ -9,7 +9,7 @@ import { companyNoState, isLoginState, loginIdState, loginNoState, memberCodeSta
 import CompanyNoNull from "./CompanyNoNull";
 import CompanyInfoUpdate from "./CompanyInfoUpdate";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CompanyProductList from "./CompanyProductList";
 import CompanyProductInsert from "./CompanyProductInsert";
 import CompanyProductUpdate from "./CompanyProductUpdate";
@@ -24,12 +24,20 @@ import CompanySalesChart from "./CompanySalesChart";
 import DayScheduleInfo from "./DayScheduleInfo";
 import DetailInfo from "./DetailInfo";
 import WeddingPayment from "./WeddingPayment";
+import CompanyJoinFrm from "./CompanyJoinFrm";
 
 const CompanyMain = () => {
 	const [companyNo, setCompanyNo] = useRecoilState(companyNoState);
 	const [loginNo, setLoginNo] = useRecoilState(loginNoState);
+	const [loginId, setLoginId] = useRecoilState(loginIdState);
+	const [memberType, setMemberType] = useRecoilState(memberTypeState);
+	const [memberCode, setMemberCode] = useRecoilState(memberCodeState);
 	const backServer = process.env.REACT_APP_BACK_SERVER;
-	console.log(companyNo);
+	const navigate = useNavigate();
+	const isLogin = useRecoilValue(isLoginState);
+	const [info, setInfo] = useState({});
+	const location = useLocation(); // 현재 경로를 확인하기 위해 useLocation 사용
+	const isCompanyPage = location.pathname;
 	useEffect(() => {
 		// 외부 스크립트 로드 함수
 		const loadScript = (src, callback) => {
@@ -53,12 +61,12 @@ const CompanyMain = () => {
 			scripts.forEach((script) => script.remove());
 		};
 	}, []);
-
 	useEffect(() => {
 		axios
 			.get(`${backServer}/company/main/${companyNo}`)
 			.then((res) => {
 				console.log(res);
+				setInfo(res.data);
 			})
 			.catch((err) => {
 				console.log(err);
@@ -69,58 +77,87 @@ const CompanyMain = () => {
 			<div className="company-wrap">
 				<div className="side-info">
 					<section className="section">
-						<div className="side-info-content">
-							<img src="/image/main_logo.png" />
-							<table className="side-tbl" border={1} style={{ borderColor: "#000" }}>
-								<thead>
+						<div className="admin-menu">
+							<div className="admin-menu-img">
+								<img src="/image/suv_logo.png" />
+							</div>
+							<table className="admin-menu-tbl">
+								<tbody>
 									<tr>
-										<th style={{ width: "50%" }}>직급 : </th>
-										<td style={{ width: "50%" }}>ㅇ </td>
+										<th>직급</th>
+										<td>{info.memberType === 2 ? "관리자" : "정보없음"}</td>
 									</tr>
 									<tr>
-										<th style={{ width: "50%" }}>이름 : </th>
-										<td style={{ width: "50%" }}>4</td>
+										<th>이름</th>
+										<td>{info.memberName}</td>
 									</tr>
-								</thead>
+									<tr>
+										<td>
+											<button
+												className="insertAdminBtn"
+												onClick={() => {
+													navigate("/question/main");
+												}}
+											>
+												고객센터
+											</button>
+										</td>
+										<td>
+											<button
+												className="logoutBtn"
+												onClick={() => {
+													setLoginNo(0);
+													setLoginId("");
+													setMemberType(-1);
+													setMemberCode("");
+													setCompanyNo(null);
+													delete axios.defaults.headers.common["Authorization"];
+													window.localStorage.removeItem("refreshToken");
+													navigate("/");
+												}}
+											>
+												로그아웃
+											</button>
+										</td>
+									</tr>
+								</tbody>
 							</table>
-							<HeaderLink />
 						</div>
-						<div className="convention">
-							<button>
-								<Link to={"/convention/main"}>박람회로 이동</Link>
-							</button>
-						</div>
+						<button
+							className="convention-btn"
+							onClick={() => {
+								navigate("/convention/main");
+							}}
+						>
+							박람회 페이지로
+						</button>
 					</section>
 				</div>
 
 				<section className="content-wrap">
-					<div>{companyNo === null || companyNo === 0 ? <CompanyNoNull /> : ""}</div>
-					{companyNo === null && companyNo === 0 ? (
-						<Routes>
-							<Route path="join" element={<CompanyJoin />} />
-							<Route path="null" element={<CompanyNoNull />} />
-						</Routes>
-					) : (
-						<Routes>
-							<Route path="null" element={<CompanyNoNull />} />
-							<Route path="join" element={<CompanyJoin />} />
-							<Route path="info" element={<CompanyInfo />} />
-							<Route path="info/update/" element={<CompanyInfoUpdate />} />
-							<Route path="product/list" element={<CompanyProductList />} />
-							<Route path="product/insert" element={<CompanyProductInsert />} />
-							<Route path="customer" element={<CompanyCustomer />} />
-							<Route path="counsel" element={<CompanyCounsel />} />
-							<Route path="product/update/:productNo" element={<CompanyProductUpdate />} />
-							<Route path="advertisement" element={<CompanyAdvertisement />} />
-							<Route path="advertisementRequest" element={<AdvertisementRequest />} />
-							<Route path="sales" element={<CompanySalesChart />} />
-							<Route path="schedule" element={<CompanySchedule />} />
-							<Route path="notice/*" element={<CompanyNotice />} />
-							<Route path="schedule/dayInfo" element={<DayScheduleInfo />} />
-							<Route path="schedule/dayInfo/detailInfo" element={<DetailInfo />} />
-							<Route path="WeddingHallPayMent/:productNo" element={<WeddingPayment />} />
-						</Routes>
-					)}
+					{/* companyNo가 null이거나 0인 경우이면서 경로가 /company로 시작할 때만 NoNull 컴포넌트 표시 */}
+					{(companyNo === null || companyNo === 0) && isCompanyPage !== "/company/join" ? <CompanyNoNull /> : null}
+
+					<Routes>
+						{/* 기존의 라우팅 경로 설정 */}
+						<Route path="null" element={<CompanyNoNull />} />
+						<Route path="join" element={<CompanyJoin />} />
+						<Route path="info" element={<CompanyInfo />} />
+						<Route path="info/update/" element={<CompanyInfoUpdate />} />
+						<Route path="product/list" element={<CompanyProductList />} />
+						<Route path="product/insert" element={<CompanyProductInsert />} />
+						<Route path="customer" element={<CompanyCustomer />} />
+						<Route path="counsel" element={<CompanyCounsel />} />
+						<Route path="product/update/:productNo" element={<CompanyProductUpdate />} />
+						<Route path="advertisement" element={<CompanyAdvertisement />} />
+						<Route path="advertisementRequest" element={<AdvertisementRequest />} />
+						<Route path="sales" element={<CompanySalesChart />} />
+						<Route path="schedule" element={<CompanySchedule />} />
+						<Route path="notice/*" element={<CompanyNotice />} />
+						<Route path="schedule/dayInfo" element={<DayScheduleInfo />} />
+						<Route path="schedule/dayInfo/detailInfo" element={<DetailInfo />} />
+						<Route path="WeddingHallPayMent/:productNo" element={<WeddingPayment />} />
+					</Routes>
 				</section>
 			</div>
 		</>
@@ -147,17 +184,15 @@ const HeaderLink = () => {
 	return (
 		<>
 			{isLogin ? (
-				<div className="logout">
+				<div className="login">
 					<Link to="/logout" onClick={logout}>
-						<div>로그아웃</div>
+						로그아웃
 					</Link>
 					<span>|</span>
-					<Link to="#">
-						<div>고객센터</div>
-					</Link>
+					<Link to="#">고객센터</Link>
 				</div>
 			) : (
-				<div className="login">
+				<div className="logout">
 					<Link to="/join/agree">회원가입</Link>
 					<span>|</span>
 					<Link to="#">고객센터</Link>
