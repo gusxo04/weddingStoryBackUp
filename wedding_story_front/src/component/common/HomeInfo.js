ffed9b0e32e4ef1c5086a40c45c48b6bed8a83dc
seong <tpdud3449@naver.com>
2024-10-10 22:14:57 +0900

    commit

--- wedding_story_front/src/component/common/HomeInfo.js
+++ wedding_story_front/src/component/common/HomeInfo.js
@@ -1,45 +1,65 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const HomeInfo = () => {
-	const [currentIndex1, setCurrentIndex1] = useState(0);
-	const [currentIndex2, setCurrentIndex2] = useState(0);
 	const content = [
-		{ img: "/image/웨딩레시피메인.jpg", img1: "/image/웨딩레시피메인.jpg" },
-		{ img: "/image/웨딩레시피메인.jpg", img1: "/image/웨딩레시피메인.jpg" },
-		{ img: "/image/웨딩레시피메인.jpg", img1: "/image/웨딩레시피메인.jpg" },
+		{
+			img: "/image/웨딩홀/1/메인사진.jpg",
+			suv1: "/image/웨딩홀/1/서브사진1.jpg",
+			suv2: "/image/웨딩홀/1/서브사진2.jpg",
+			suv3: "/image/웨딩홀/1/서브사진3.jpg",
+			text: "웨딩홀 비용 Save Tip",
+		},
+		{
+			img: "/image/웨딩홀/2/메인사진.jpg",
+			suv1: "/image/웨딩홀/2/서브사진1.jpg",
+			suv2: "/image/웨딩홀/2/서브사진2.jpg",
+			suv3: "/image/웨딩홀/2/서브사진3.jpg",
+			text: "웨딩홀 필수 체크리스트!!",
+		},
+		{
+			img: "/image/웨딩홀/3/메인사진.jpg",
+			suv1: "/image/웨딩홀/3/서브사진1.jpg",
+			suv2: "/image/웨딩홀/3/서브사진2.jpg",
+			suv3: "/image/웨딩홀/3/서브사진3.jpg",
+			text: "웨딩홀 투어 전 미리 체크하면 좋은 점!",
+		},
+		{
+			img: "/image/웨딩홀/3/메인사진.jpg",
+			suv1: "/image/웨딩홀/3/서브사진1.jpg",
+			suv2: "/image/웨딩홀/3/서브사진2.jpg",
+			suv3: "/image/웨딩홀/3/서브사진3.jpg",
+			text: "웨딩홀 투어 전 미리 체크하면 좋은 점!",
+		},
 	];
-	const [isModalOpen, setIsModalOpen] = useState(false); //모달 오픈 여부
-	const [selectedEvent, setSelectedEvent] = useState(null); //선택한 이벤트의 정보가 담긴 함수
-	const openModal = (info) => {
-		console.log(info);
+	const [selectedIndex, setSelectedIndex] = useState(null); // 현재 선택된 대표사진 인덱스
+	const [showModal, setShowModal] = useState(false); // 모달 표시 여부
 
-		setSelectedEvent({
-			img1: info.img,
-		});
-		setIsModalOpen(true);
-		handlePrev(info);
-		handleNext(info);
+	// 대표 사진 클릭 시 모달 열기
+	const openModal = (index) => {
+		setSelectedIndex(index);
+		setShowModal(true);
 	};
 
+	// 모달 닫기
 	const closeModal = () => {
-		setIsModalOpen(false);
-		setSelectedEvent(null);
+		setShowModal(false);
+		setSelectedIndex(null);
 	};
-	// 왼쪽 화살표 클릭 시 이전 이미지로
+	const [startIndex, setStartIndex] = useState(0); // 현재 슬라이드 시작 인덱스
+	const itemsPerPage = 3; // 한 번에 보여줄 항목 수
+
+	// 이전 슬라이드
 	const prevSlide = () => {
-		setCurrentIndex1((prevIndex) => (prevIndex === 0 ? content.length - 1 : prevIndex - 1));
+		setStartIndex((prevIndex) => (prevIndex === 0 ? content.length - itemsPerPage : prevIndex - itemsPerPage));
 	};
 
-	// 오른쪽 화살표 클릭 시 다음 이미지로
+	// 다음 슬라이드
 	const nextSlide = () => {
-		setCurrentIndex1((prevIndex) => (prevIndex + 1) % content.length);
+		setStartIndex((prevIndex) => (prevIndex + itemsPerPage >= content.length ? 0 : prevIndex + itemsPerPage));
 	};
-	const handlePrev = (info) => {
-		setCurrentIndex2((prevIndex) => (prevIndex === 0 ? info.length - 1 : prevIndex - 1));
-	};
-	const handleNext = (info) => {
-		setCurrentIndex2((prevIndex) => (prevIndex === info.length - 1 ? 0 : prevIndex + 1));
-	};
+
+	// 현재 보여줄 아이템 슬라이스
+	const currentItems = content.slice(startIndex, startIndex + itemsPerPage);
 	return (
 		<div className="home-section2">
 			<div className="home-section2-titleBox">
@@ -54,13 +74,13 @@ const HomeInfo = () => {
 				<button className="arrow2 left" onClick={prevSlide}>
 					&#10094; {/* 왼쪽 화살표 기호 */}
 				</button>
-				{content.map((info) => (
-					<div onClick={() => openModal(info)}>
+				{currentItems.map((info, index) => (
+					<div key={index} onClick={() => openModal(startIndex + index)}>
 						<div className="home-section2-infoImg">
-							<img src={info.img} />
+							<img src={info.img} alt="웨딩 이미지" />
 						</div>
 						<div className="home-section2-infoInfo">
-							<h4>웨딩홀 타입별 드레스 TIP</h4>
+							<h4>{info.text}</h4>
 						</div>
 					</div>
 				))}
@@ -70,27 +90,45 @@ const HomeInfo = () => {
 				</button>
 			</div>
 			{/* 모달 */}
-			{isModalOpen ? (
-				<div className="home-infoDetail-wrap">
-					<div className="home-infoDetail-slider" onClick={handlePrev}>
-						<span class="material-icons">chevron_left</span>
-					</div>
-					<div style={{ height: "100%" }}>
-						<span className="home-infoDetail-close" onClick={closeModal}>
-							&times;
-						</span>
-						<div className="home-infoDetail">
-							<img src={selectedEvent[currentIndex2]} alt={`Slide ${currentIndex2 + 1}`} />
-						</div>
-					</div>
-					<div className="home-infoDetail-slider" onClick={handleNext}>
-						<span class="material-icons">navigate_next</span>
-					</div>
-				</div>
-			) : (
-				""
-			)}
+			{showModal && <Modal content={content[selectedIndex]} closeModal={closeModal} />}
 		</div>
 	);
 };
 export default HomeInfo;
+
+function Modal({ content, closeModal }) {
+	console.log(content);
+
+	const [currentSlide, setCurrentSlide] = useState(0); // 현재 슬라이드 번호
+	const subImages = [content.img, content.suv1, content.suv2, content.suv3]; // 서브 이미지 배열
+
+	// 이전 슬라이드로 이동
+	const prevSlide = () => {
+		setCurrentSlide((prev) => (prev === 0 ? subImages.length - 1 : prev - 1));
+	};
+
+	// 다음 슬라이드로 이동
+	const nextSlide = () => {
+		setCurrentSlide((prev) => (prev === subImages.length - 1 ? 0 : prev + 1));
+	};
+
+	return (
+		<div className="home-infoDetail-wrap">
+			<div className="modal-content">
+				<span className="home-infoDetail-close" onClick={closeModal}>
+					&times;
+				</span>
+
+				<span className="arrow prev" onClick={prevSlide}>
+					&#10094;
+				</span>
+
+				<img src={subImages[currentSlide]} alt={`서브 사진 ${currentSlide + 1}`} className="modal-img" />
+
+				<span className="arrow next" onClick={nextSlide}>
+					&#10095;
+				</span>
+			</div>
+		</div>
+	);
+}
