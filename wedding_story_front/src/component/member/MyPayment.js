6723f1fc74fefd8dd27c899a05625c980c7ed831
rlawl135 <rlawl135@gmail.com>
2024-10-11 09:27:41 +0900

    수정

--- wedding_story_front/src/component/member/MyPayment.js
+++ wedding_story_front/src/component/member/MyPayment.js
@@ -163,7 +163,6 @@ const MyPayment = (props) => {
 														환불요청
 													</button>
 												)}
-												<Review payNo={pay.payNo} productNo={pay.productNo} index={index} />
 											</div>
 										</td>
 									</tr>
