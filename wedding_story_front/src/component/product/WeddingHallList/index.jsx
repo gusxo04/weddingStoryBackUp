9e6f9746a1e89b90c3f56a4faf4d32189de2ac2a
rlawl135 <rlawl135@gmail.com>
2024-10-11 09:23:20 +0900

    웨딩좋아요수정

--- wedding_story_front/src/component/product/WeddingHallList/index.jsx
+++ wedding_story_front/src/component/product/WeddingHallList/index.jsx
@@ -72,18 +72,21 @@ const BoardItem = (props) => {
 	const memberNo = useRecoilValue(loginNoState);
 	const navigate = useNavigate();
 	const [liked, setLiked] = useState(false);
-
 	// 서버에서 좋아요 상태 받아오기
 	useEffect(() => {
 		axios
-			.get(`${backServer}/product/favorite/${productNo}/${memberNo}`)
+			.get(`${backServer}/product/favoriteState/${productNo}/${memberNo}`)
 			.then((res) => {
-				setLiked(res.data.liked); // 서버에서 받은 좋아요 상태 설정
+				if (res.data > 0) {
+					setLiked(true);
+				} else {
+					setLiked(false);
+				}
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-	}, [productNo, memberNo]);
+	}, []);
 
 	const handleLikeToggle = (e) => {
 		//console.log("실행됨");
